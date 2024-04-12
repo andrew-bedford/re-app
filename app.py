d99c18fa6bb5eb0087b251502430d489705ade11
Andrew Bedford <email@andrew-bedford.ca>
2024-04-12 08:29:10 -0400

    (WIP): Attempt to display splashscreen while loading
    When the server is not already running (i.e., first execution), we
    should display a splashscreen while it's loading. This is not quite working
    yet.

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
+import subprocess
+import time
 from PyQt6 import QtWidgets, QtWebEngineWidgets, QtWebEngineCore, QtCore
-from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout
+from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QSplashScreen, QLabel
 from PyQt6.QtWebEngineWidgets import QWebEngineView
 from PyQt6.QtWebEngineCore import QWebEnginePage
 from PyQt6.QtCore import QUrl
-from PyQt6.QtGui import QDesktopServices
+from PyQt6.QtGui import QDesktopServices, QPixmap
 
 import os
 import sys
@@ -33,28 +35,48 @@ class OpenLinksInDesktopBrowserWebEnginePage(QWebEnginePage):
 
         return super().acceptNavigationRequest(url, navType, isMainFrame)
 
+class StartServerWorker(QRunnable):
+    @pyqtSlot()
+    def run(self):
+        print("Thread start")
+        time.sleep(5)
+        print("Thread complete")
+
 class MainWindow(QtWidgets.QMainWindow):
 
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
-
-        # l = QVBoxLayout() 
-        # l.addWidget(view)
-        # self.setLayout(l)
+        self.setWindowTitle("re/window")
+        self.resize(1280, 800)
 
         self.browser = QWebEngineView()
-        self.setWindowTitle("re/window")
         self.browser.setPage(OpenLinksInDesktopBrowserWebEnginePage(self))
 
+        label = QLabel(self)
+        icon = QPixmap('icon.png')
+        label.setPixmap(icon)
+        label.setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
+        self.setCentralWidget(label)
+        self.show()
+        
         if (not isReachable("http://localhost:7000/")):
-            os.system("dotnet run --project /re/log")
-        sleep(1000)
+            subprocess.Popen(["dotnet", "run", "--project", "/re/log"])
+        while not isReachable("http://localhost:7000/"):
+            QApplication.processEvents()
+            time.sleep(1)
+
         self.browser.setUrl(QUrl("http://localhost:7000/")) 
         self.setCentralWidget(self.browser)
-        self.show()
+        
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
+    # pixmap = QPixmap("/re/window/icon.png")
+    # splash = QSplashScreen(pixmap)
+    # splash.show()
+    # app.processEvents()
+
+    # splash.close()
     w = MainWindow()
     w.show()
     app.exec()
