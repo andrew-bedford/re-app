9d714e34bbe08f15b2bf6fc55471c365e7479e91
Andrew Bedford <email@andrew-bedford.ca>
2024-04-27 16:24:25 -0400

    Use relative path for icon file
    Relative to the path of the script itself.

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
-import subprocess
-import time
-from PyQt6 import QtWidgets, QtWebEngineWidgets, QtWebEngineCore, QtCore
-from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QSplashScreen, QLabel
-from PyQt6.QtWebEngineWidgets import QWebEngineView
-from PyQt6.QtWebEngineCore import QWebEnginePage
-from PyQt6.QtCore import QUrl, QTimer
-from PyQt6.QtGui import QDesktopServices, QPixmap, QIcon
-
 import os
 import sys
 import requests
+import subprocess
+
+from PyQt6 import QtWidgets, QtWebEngineWidgets, QtWebEngineCore, QtCore
+from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QSplashScreen, QLabel
+from PyQt6.QtWebEngineWidgets import QWebEngineView
+from PyQt6.QtWebEngineCore import QWebEnginePage, QWebEngineSettings
+from PyQt6.QtCore import QUrl, QTimer
+from PyQt6.QtGui import QDesktopServices, QPixmap, QIcon
+
+
+
 
 def isReachable(url):
     try:
@@ -50,11 +52,23 @@ class MainWindow(QtWidgets.QMainWindow):
         super(MainWindow, self).__init__(*args, **kwargs)
         self.setWindowTitle("re/window")
         self.resize(1280, 800)
-        self.icon = QPixmap('icon.png')
+        self.icon = QPixmap(os.path.join(workingDirectory, 'icon.png'))
         self.setWindowIcon(QIcon(self.icon))
 
         self.browser = QWebEngineView()
         self.browser.setPage(OpenLinksInDesktopBrowserWebEnginePage(self))
+
+        # Attemps at making local storage work, it might be better to just save the data to a local file instead.
+        self.browser.settings().setAttribute(QWebEngineSettings.WebAttribute.LocalStorageEnabled, True)
+        self.browser.settings().setAttribute(QWebEngineSettings.WebAttribute.PdfViewerEnabled, True)
+        self.browser.settings().setAttribute(QWebEngineSettings.WebAttribute.PluginsEnabled, True)
+        self.browser.settings().setAttribute(QWebEngineSettings.WebAttribute.AllowRunningInsecureContent, True)
+        self.browser.settings().setAttribute(QWebEngineSettings.WebAttribute.FullScreenSupportEnabled, True)
+        self.browser.settings().setAttribute(QWebEngineSettings.WebAttribute.Accelerated2dCanvasEnabled, True)
+        self.browser.settings().setAttribute(QWebEngineSettings.WebAttribute.LocalContentCanAccessFileUrls, True)
+        self.browser.settings().setAttribute(QWebEngineSettings.WebAttribute.JavascriptCanAccessClipboard, True)
+
+        self.browser.page().quotaRequested.connect(lambda request: request.accept())
 
         self.showSplashscreen()
         
@@ -66,7 +80,8 @@ class MainWindow(QtWidgets.QMainWindow):
         self.timer.start(100)
 
 if __name__ == '__main__':
+    workingDirectory = os.path.abspath(os.path.dirname(__file__))
     app = QtWidgets.QApplication(sys.argv)
-    w = MainWindow()
-    w.show()
+    window = MainWindow()
+    window.show()
     app.exec()
